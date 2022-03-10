cc7caf5f136ead0a21434ff5a952fb7c896ee13a
ac-gomes <acgoms88@gmail.com>
2022-03-09 21:42:31 -0300

    Query Parameters

--- myapi.py
+++ myapi.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI, Path
 
 app = FastAPI()
@@ -24,3 +25,21 @@ def index():
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="Retrive the student ID", gt=0)):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student_name(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"data": "Not found"}
+
+
+# Recommended pattern for queries on non-mandatory fields
+# Combining riquired field with non-mandatory field in the query
+@app.get("/get-by-name-optional")
+def get_student_name_optional(*,name: Optional[str] = None, age: int):
+    for student_id in students:
+        if students[student_id]["name"] == name or students[student_id]["age"] == age:
+            return students[student_id]
+    return {"data": "Not found"}
